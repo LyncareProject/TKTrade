41ad4c3d6cd1bb7f79c712c136f158ae14db12d6
박은찬 <dmscksqkr1@gmail.com>
2023-08-04 11:48:06 +0900

    제품 불러오기 수정

--- client/src/pages/Product/ProductEditor.jsx
+++ client/src/pages/Product/ProductEditor.jsx
@@ -10,8 +10,9 @@ const ProductEditor = ({ setMode })=>{
     const { state } = useLocation();
     const navigate = useNavigate()
 
-    const [ mainCategory, setMainCategory ] = useState([])
-    const [ subCategory, setSubCategory ] = useState([])
+    const [ loading, setLoading ] = useState(true)
+    const [ mainCategory, setMainCategory ] = useState(null)
+    const [ subCategory, setSubCategory ] = useState(null)
     const [ checkedCategory, setCheckedCategory ] = useState('')
     const [ filteredSub, setFilteredSub ] = useState([])
 
@@ -27,6 +28,7 @@ const ProductEditor = ({ setMode })=>{
         images : []
     });
     const { _id, nameEng, name, category, category_2, content, contentEng, pdf, images } = input
+
     const handleInput = (e)=>{
         setInput({
             ...input,
@@ -59,39 +61,43 @@ const ProductEditor = ({ setMode })=>{
             })
             .catch(err => console.log(err))
     }
-    const fetchMainCategory = async ()=>{
-        await readCategory()
-            .then(response => setMainCategory(response.data))
-            .catch(err => console.log(err.messages))
-    }
-    const fetchAllSubCategory = async () => {
-        await readAllSubcategory()
-            .then(response => setSubCategory(response.data))
-            .catch(err => console.log(err.messages))
-    }
 
     useEffect(()=>{
+        setLoading(true)
         setMode('product')
-        fetchMainCategory()
-        fetchAllSubCategory()
-        if(state){
-            findOneProduct({ _id : state })
-                .then(response => {
-                    setCheckedCategory(response.data.category)
-                    setInput(response.data)
-                })
+        const fetchCategoryData = async ()=>{
+            await readCategory()
+                .then(response => setMainCategory(response.data))
                 .catch(err => console.log(err.messages))
+            await readAllSubcategory()
+                .then(response => setSubCategory(response.data))
+                .catch(err => console.log(err.messages))
+            setLoading(false)
         }
-
+        fetchCategoryData()
     }, [])
+    useEffect(()=>{
+        if(state && mainCategory && subCategory){
+            const fetchData = async ()=>{
+                await findOneProduct({ _id : state })
+                    .then(response => {
+                        setCheckedCategory(response.data.category)
+                        setInput(response.data)
+                    })
+                    .catch(err => console.log(err.messages))
+            }
+            fetchData()
+        }
+    },[state, loading])
     useEffect(()=>{
         if(checkedCategory){
             const filter = subCategory.filter(sub => 
                 sub.category === checkedCategory
             )
-            return setFilteredSub(filter)
+            setFilteredSub(filter)
         }
-    }, [checkedCategory])
+    },[checkedCategory])
+
     const createProductBtn = ()=>{
         if(!nameEng || !name || !category || !category_2 || !content || !contentEng){
             return alert('필수 정보들을 모두 입력해주세요')
@@ -139,95 +145,100 @@ const ProductEditor = ({ setMode })=>{
         })
         .catch(err => alert(err.message.message))
     }
+    
     return(
-        <div className='ProductEditor'>
-            <div className="Wrap">
-                <div className="labelInput">
-                    <label className="EditorLabel" htmlFor="name">한글명</label>
-                    <input className="EditorInput" type="text" value={ name } id="name" name="name" onChange={ handleInput }/>
-                </div>
-                <div className="labelInput">
-                    <label className="EditorLabel" htmlFor="nameEng">영문명</label>
-                    <input className="EditorInput" type="text" value={ nameEng } id="nameEng" name="nameEng" onChange={ handleInput }/>
-                </div>
-                <div className="labelInput">
-                    <label className="EditorLabel" htmlFor="category">카테고리 대분류</label>
-                    <select name="category" id="category" value={ category } onChange={(e)=>{
-                        setCheckedCategory(e.target.value)
-                        handleInput(e)
-                    }}>
-                        <option value=''>상위 카테고리</option>
-                        {
-                            mainCategory.map((a, i)=>
-                                <option key={ i } value={ a.category }>{ a.category }</option>
-                            )
-                        }
-                    </select>
-                </div>
-                <div className="labelInput">
-                    <label className="EditorLabel" htmlFor="category_2">카테고리 소분류</label>
-                    <select name="category_2" id="category_2" value={ category_2 } onChange={ handleInput }>
+        <>
+            {
+                loading
+                ? <p>Loading</p>
+                :         <div className='ProductEditor'>
+                <div className="Wrap">
+                    <div className="labelInput">
+                        <label className="EditorLabel" htmlFor="name">한글명</label>
+                        <input className="EditorInput" type="text" value={ name } id="name" name="name" onChange={ handleInput }/>
+                    </div>
+                    <div className="labelInput">
+                        <label className="EditorLabel" htmlFor="nameEng">영문명</label>
+                        <input className="EditorInput" type="text" value={ nameEng } id="nameEng" name="nameEng" onChange={ handleInput }/>
+                    </div>
+                    <div className="labelInput">
+                        <label className="EditorLabel" htmlFor="category">카테고리 대분류</label>
+                        <select name="category" id="category" value={ category } onChange={(e)=>{
+                            setCheckedCategory(e.target.value)
+                            handleInput(e)
+                        }}>
+                            <option value=''>상위 카테고리</option>
+                            {
+                                mainCategory.map((category, index)=>
+                                    <option key={ index } value={ category.category }>{ category.category }</option>
+                                )
+                            }
+                        </select>
+                    </div>
+                    <div className="labelInput">
+                        <label className="EditorLabel" htmlFor="category_2">카테고리 소분류</label>
                         {
                             !checkedCategory
-                            ? <>
+                            ? <select name="category_2" id="category_2" value={ category_2 } onChange={ handleInput }>
                                 <option value=''>상위 카테고리를 선택해주세요.</option>
                                 {
-                                    subCategory.map((a, i)=>
-                                        <option key={ i } value={ a.subcategory }>{ a.subcategory }</option>
+                                    subCategory.map((category, index)=>
+                                        <option key={ index } value={ category.subcategory }>{ category.subcategory }</option>
                                     )
                                 }
-                            </>
+                            </select>
+                            : <select name="category_2" id="category_2" value={ category_2 } onChange={ handleInput }>
+                            {
+                                filteredSub.map((category, index)=>
+                                    <option key={ index } value={ category.subcategory }>{ category.subcategory }</option>
+                                )
+                            }
+                        </select>
+                        }
+                    </div>
+
+                    <div className="labelInput">
+                        <label className="EditorLabel" htmlFor="content">한글 내용</label>
+                        <textarea name="content" id="content" cols="30" rows="10" value={ content } onChange={ handleInput }></textarea>
+                    </div>
+                    <div className="labelInput">
+                        <label className="EditorLabel" htmlFor="contentEng">영문 내용</label>
+                        <textarea name="contentEng" id="contentEng" cols="30" rows="10" value={ contentEng } onChange={ handleInput }></textarea>
+                    </div>
+                    <div className="labelInput">
+                        <label className="EditorLabel" htmlFor="pdf">PDF 링크</label>
+                        <input className="EditorInput" type="text" value={ pdf } id="pdf" name="pdf" onChange={ handleInput }/>
+                    </div>
+                    <h3 className="EditorLabel">이미지</h3>
+                    <div className="ImgLists">
+                        {
+                            images.map((image, index)=>
+                                <div className="ImgWrap" key={ index } onClick={ ()=>{ handleRemoveImage(index) } }>
+                                    <img src={`${ testUrl }/${ image }`} alt="" />
+                                </div>
+                            )
+                        }
+                        {
+                            images.length === 5
+                            ? null
                             : <>
-                                <option value=''>하위 카테고리를 선택해주세요.</option>
-                                {
-                                    filteredSub.map((a, i)=>
-                                        <option key={ i } value={ a.subcategory }>{ a.subcategory }</option>
-                                    )
-                                }
+                                <label htmlFor="file" className="ImgWrap">
+                                    <div className="btn-upload">+</div>
+                                </label>
+                                <input type="file" id="file" className="Hide" onChange={ handleImage } />
                             </>
                         }
-                    </select>
-                </div>
-                <div className="labelInput">
-                    <label className="EditorLabel" htmlFor="content">한글 내용</label>
-                    <textarea name="content" id="content" cols="30" rows="10" value={ content } onChange={ handleInput }></textarea>
-                </div>
-                <div className="labelInput">
-                    <label className="EditorLabel" htmlFor="contentEng">영문 내용</label>
-                    <textarea name="contentEng" id="contentEng" cols="30" rows="10" value={ contentEng } onChange={ handleInput }></textarea>
-                </div>
-                <div className="labelInput">
-                    <label className="EditorLabel" htmlFor="pdf">PDF 링크</label>
-                    <input className="EditorInput" type="text" value={ pdf } id="pdf" name="pdf" onChange={ handleInput }/>
-                </div>
-                <h3 className="EditorLabel">이미지</h3>
-                <div className="ImgLists">
+                        
+                    </div>
                     {
-                        images.map((image, index)=>
-                            <div className="ImgWrap" key={ index } onClick={ ()=>{ handleRemoveImage(index) } }>
-                                <img src={`${ testUrl }/${ image }`} alt="" />
-                            </div>
-                        )
-                    }
-                    {
-                        images.length === 5
-                        ? null
-                        : <>
-                            <label for="file" className="ImgWrap">
-                                <div class="btn-upload">+</div>
-                            </label>
-                            <input type="file" id="file" className="Hide" onChange={ handleImage } />
-                        </>
-                    }
-                    
+                        !state
+                        ? <button className="ProductEditorBtn" onClick={ createProductBtn }>제품 등록</button>
+                        : <button className="ProductEditorBtn" onClick={ updateProductBtn }>제품 수정</button>
+                    } 
                 </div>
-                {
-                    !state
-                    ? <button className="ProductEditorBtn" onClick={ createProductBtn }>제품 등록</button>
-                    : <button className="ProductEditorBtn" onClick={ updateProductBtn }>제품 수정</button>
-                } 
             </div>
-        </div>
+            }
+        </>
     )
 }
 
