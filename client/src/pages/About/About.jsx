76471dc16e7180a5188ea9e6389a927c27cbb028
박은찬 <dmscksqkr1@gmail.com>
2023-09-06 15:03:52 +0900

    about 줄간격 조절

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -6,8 +6,8 @@ const About = ()=>{
         <div className='Wrap'>
             <h3 className='About_h3'>ABOUT</h3>
             <div id='About_Wrap' className="Wrap About_Wrap">
-                <p>We, establish for the purpose to contribute a part of the quality items worldwide, especially to whom engage for a plant<br /> which is related with Condensor, Boiler, Air-conditioner, Air Fin-cooler, Heat Exchanger fabricator.<br />
-                Please enjoy any material here and You can find the Qualified Tube expanders, Beveling-machine, Expanding-machine on time through our seasoned experiences.<br />
+                <p>We, establish for the purpose to contribute a part of the quality items worldwide, especially to whom engage for a plant which is related with Condensor, Boiler, Air-conditioner, Air Fin-cooler, Heat Exchanger fabricator.<br /><br />
+                Please enjoy any material here and You can find the Qualified Tube expanders, Beveling-machine, Expanding-machine on time through our seasoned experiences.<br /><br />
                 We are waiting you and hope that we have related with long-term connection of you.</p>
                 <p className='About_p1'>Yours,</p>
                 <p className='About_p2'>B.Y. KIM</p>
